e120c0c5aff7fb07a80f06bbaa536ffe3f92f694
KibetBrian <briankibet2040@gmail.com>
2022-12-13 15:26:51 +0300

    add update department route

--- routes/Department.js
+++ routes/Department.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { GetAllDepartments, RegisterDepartment, UpdateDepartmentStatus } from "../controllers/Department.js";
+import { GetAllDepartments, RegisterDepartment, UpdateDepartment, UpdateDepartmentStatus } from "../controllers/Department.js";
 
 const departmentRouter = Router();
 
 departmentRouter.get('/all', GetAllDepartments);
-departmentRouter.put('/update-status/:action/:name', UpdateDepartmentStatus)
 departmentRouter.post('/register', RegisterDepartment);
+departmentRouter.put('/update-status/:action/:name', UpdateDepartmentStatus);
+departmentRouter.put('/update/:name', UpdateDepartment);
 
 export default departmentRouter;
 
