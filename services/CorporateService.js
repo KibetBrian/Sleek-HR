8c737e7c58391a14d23e872756595b7dcae0a57a
KibetBrian <briankibet2040@gmail.com>
2022-12-13 10:29:19 +0300

    add coporate service class

---
+++ services/CorporateService.js
@@ -0,0 +1,46 @@
+import Corporate from "../models/Corporate.js";
+import { DB_ERROR_CODES } from "../utils/utils.js";
+
+class CorporateService {
+
+    constructor() {
+        this.corporate = Corporate;
+    }
+
+    async registerCorporate(data) {
+        try {
+            const response = await this.corporate.create(data);
+            if (response) {
+                return {
+                    success: true,
+                    status: 200,
+                    message: "Corporate registered",
+                    corporateData: response
+                }
+            }
+        } catch (e) {
+            if (e.original.code === DB_ERROR_CODES.UCV) {
+                return {
+                    success: false,
+                    status: 409,
+                    message: "Choose corporate with different name"
+                }
+            }
+
+        }
+    }
+
+    async getAllCorporates() {
+        try {
+            const response = await this.corporate.findAll();
+            return {
+                success: true,
+                status: 200,
+                corporates: response
+            }
+        } catch (e) {
+
+        }
+    }
+}
+export { CorporateService }
