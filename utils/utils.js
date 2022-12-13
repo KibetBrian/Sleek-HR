e9965c2189f7a54d170e1c2a6dc0343d06981cb7
KibetBrian <briankibet2040@gmail.com>
2022-12-13 17:37:54 +0300

    find all department util

--- utils/utils.js
+++ utils/utils.js
@@ -1,5 +1,22 @@
+import Department from "../models/Department";
+
+const findAllDepartmentNames = async () => {
+    let departmentNames = [];
+    const departments = await Department.findAll();
+    for (let i = 0; i < departments.length; i++) {
+        departmentNames.push(departments[i].name);
+    };
+
+    return departmentNames;
+};
+
+const COMPENSATION_TYPES = {
+    HOURLY: 'hourly',
+    MONTHLY: 'monthly',
+}
+
 const DB_ERROR_CODES = {
     UCV: '23505'
 }
 
-export { DB_ERROR_CODES }
+export {findAllDepartmentNames, DB_ERROR_CODES }
