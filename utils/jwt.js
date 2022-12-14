d4d2fb124e639dfc0d6a3e79c582a15de8ffa96b
KibetBrian <briankibet2040@gmail.com>
2022-12-14 13:52:00 +0300

    add generate jwt token function

--- utils/jwt.js
+++ utils/jwt.js
@@ -2,21 +2,15 @@ import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const GenerateTokens = (user) => {
+const GenerateTokens = async (user) => {
     try {
-        const accessToken = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '15m' });
+        const accessToken = jwt.sign({ userId: user.id }, process.env.JWT_SECRET_KEY, { expiresIn: '15m' });
 
-        const refreshToken = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '3d' });
+        const refreshToken = jwt.sign({ userId: user.id }, process.env.JWT_SECRET_KEY, { expiresIn: '3d' });
 
         return Promise.resolve({ refreshToken, accessToken });
     } catch (e) {
         return Promise.reject(e);
     }
 };
-
-const VerifyTokens = (token) => {
-    const result = jwt.verify(token, process.env.JWT_SECRET);
-    
-}
-
 export { GenerateTokens };
