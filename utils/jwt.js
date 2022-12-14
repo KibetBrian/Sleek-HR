a696d9f80878d40fa0e1d208bd1af2c0c5a7b0e5
KibetBrian <briankibet2040@gmail.com>
2022-12-14 15:45:04 +0300

    add email to token payload

--- utils/jwt.js
+++ utils/jwt.js
@@ -4,9 +4,9 @@ dotenv.config();
 
 const GenerateTokens = async (user) => {
     try {
-        const accessToken = jwt.sign({ userId: user.id }, process.env.JWT_SECRET_KEY, { expiresIn: '15m' });
+        const accessToken = jwt.sign({ userId: user.id, email: user.workEmail }, process.env.JWT_SECRET_KEY, { expiresIn: '15m' });
 
-        const refreshToken = jwt.sign({ userId: user.id }, process.env.JWT_SECRET_KEY, { expiresIn: '3d' });
+        const refreshToken = jwt.sign({ userId: user.id, email: user.workEmail }, process.env.JWT_SECRET_KEY, { expiresIn: '3d' });
 
         return Promise.resolve({ refreshToken, accessToken });
     } catch (e) {
