e2fb6c574534fecaf6032eb235bb44a2b75d8e92
KibetBrian <briankibet2040@gmail.com>
2022-12-14 13:53:34 +0300

    update verb

--- routes/Auth.js
+++ routes/Auth.js
@@ -1,8 +1,8 @@
 import { Router } from 'express';
-import Login from '../controllers/Auth';
+import Login from '../controllers/Auth.js';
 
 const authRouter = Router();
 
-authRouter.get("/login", Login);
+authRouter.post("/login", Login);
 
 export default authRouter;
