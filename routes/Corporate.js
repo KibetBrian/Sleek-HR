d0c552b869fff60936876faba82299d7708562a1
KibetBrian <briankibet2040@gmail.com>
2022-12-14 16:37:44 +0300

    add authorization middleware

--- routes/Corporate.js
+++ routes/Corporate.js
@@ -1,10 +1,11 @@
 
 import { Router } from "express";
+import { auth, authorize } from "../middlewares/VerifyToken.js";
 import { GetAllCorporates, RegisterCorporateController } from "../controllers/Corporate.js";
 
 const corporateRouter = Router();
 
-corporateRouter.get('/all', GetAllCorporates);
+corporateRouter.get('/all', auth, authorize(["read"], ["admin", "hr"]), GetAllCorporates);
 corporateRouter.post('/register', RegisterCorporateController)
 
 
