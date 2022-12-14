19f405784eecd1f8dccdd805e2c7ea0e01317a52
KibetBrian <briankibet2040@gmail.com>
2022-12-14 16:42:21 +0300

    add authorization

--- routes/Employee.js
+++ routes/Employee.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
+import { auth, authorize } from "../middlewares/VerifyToken";
 
 const employeeRouter = Router();
 
-employeeRouter.post('/register/basic',BasicDetailsEmployeeRegistration )
+employeeRouter.post('/register/basic',auth, authorize(["read", "create", "update", "delete"], ["hr"]) BasicDetailsEmployeeRegistration )
 
 export default employeeRouter;
 
