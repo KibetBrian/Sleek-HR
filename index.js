400c442bfc63c00aefc67e7a223b7f1278679dc3
KibetBrian <briankibet2040@gmail.com>
2022-12-15 17:29:18 +0300

    add cors and mogan middlewares

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import sequelize from './config/db.js';
+import morgan from 'morgan';
+import cors from 'cors';
 
 //--------ROUTES IMPORTS------ //
 import corporateRouter from './routes/Corporate.js';
@@ -10,26 +12,22 @@ import authRouter from './routes/Auth.js';
 
 dotenv.config();
 
-const rootUrl = '/api/v1/hrm/'
+const rootUrl = '/api/v1/hrms/'
 const PORT = process.env.PORT || 8080;
 const app = express();
 
 //--------MIDDLEWARES---------//
 app.use(express.json());
+app.use(cors());
+app.use(morgan('tiny'))
 
-app.get('/', (req, res) => {
-    res.status(200).json({
-        success: true,
-        message: "Application working",
-    })
-});
 
 
 //-----------ROUTES----------//
-app.use(rootUrl+'auth', authRouter);
+app.use(rootUrl + 'auth', authRouter);
 app.use(rootUrl + 'corporate', corporateRouter);
 app.use(rootUrl + 'branch', branchRouter);
-app.use(rootUrl+'department', departmentRouter);
+app.use(rootUrl + 'department', departmentRouter);
 
 
 //-----------Database Syncing and starting server---------//
