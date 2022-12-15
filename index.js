274f53545f37ba1b696ae4eea90c8d0b0abc18a3
KibetBrian <briankibet2040@gmail.com>
2022-12-15 21:16:00 +0300

    prettify

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ import express from 'express';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 import cors from 'cors';
+
+
+//---------Local module imports-------//
 import sequelize from './config/db.js';
 import winston from './config/winston.js';
 
-//--------ROUTES IMPORTS------ //
+
+//--------Routes imports------ //
 import corporateRouter from './routes/Corporate.js';
 import branchRouter from './routes/Branch.js';
 import departmentRouter from './routes/Department.js';
@@ -13,19 +17,19 @@ import authRouter from './routes/Auth.js';
 
 dotenv.config();
 
+//-------Application Variables-------//
 const rootUrl = '/api/v1/hrms/'
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-//--------MIDDLEWARES---------//
+
+//--------Middlewares---------//
 app.use(express.json());
 app.use(cors());
-app.use(morgan('combined', {stream: winston.stream}));
+app.use(morgan('combined', { stream: winston.stream }));
 
 
-
-
-//-----------ROUTES----------//
+//-----------Routes----------//
 app.use(rootUrl + 'auth', authRouter);
 app.use(rootUrl + 'corporate', corporateRouter);
 app.use(rootUrl + 'branch', branchRouter);
@@ -37,5 +41,5 @@ sequelize.sync().then(() => {
     app.listen(PORT, () => console.log("Server started"))
 }).catch((e) => {
     console.log("Sequelize sync error", e)
-})
+});
 
